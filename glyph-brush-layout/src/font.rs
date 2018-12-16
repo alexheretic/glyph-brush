760cda8e1a2033df7511ca5f6ec14954416de56c
Alex Butler <alexheretic@gmail.com>
2018-12-16 13:55:35 +0000

    Add FontMap impl vec, array, slice tests

--- glyph-brush-layout/src/font.rs
+++ glyph-brush-layout/src/font.rs
@@ -18,3 +18,50 @@ where
         &self.as_ref()[i.0]
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    lazy_static::lazy_static! {
+        static ref DEJA_VU: Font<'static> =
+            Font::from_bytes(include_bytes!("../../fonts/DejaVuSans.ttf") as &[u8]).unwrap();
+        static ref GARAMOND: Font<'static> = {
+            let font = Font::from_bytes(include_bytes!("../../fonts/GaramondNo8-Reg.ttf") as &[u8])
+                .unwrap();
+            assert_ne!(font.glyph_count(), DEJA_VU.glyph_count());
+            font
+        };
+    }
+
+    macro_rules! assert_eq_font {
+        ($fa:expr, $fb:expr) => {
+            assert_eq!(
+                $fa.glyph_count(),
+                $fb.glyph_count(),
+                "Unexpected glyph_count"
+            );
+        };
+    }
+
+    #[test]
+    fn font_map_for_vecs() {
+        let fonts: Vec<Font> = vec![DEJA_VU.clone(), GARAMOND.clone()];
+        assert_eq_font!(fonts.font(FontId(0)), DEJA_VU);
+        assert_eq_font!(fonts.font(FontId(1)), GARAMOND);
+    }
+
+    #[test]
+    fn font_map_for_arrays() {
+        let fonts: [Font; 2] = [DEJA_VU.clone(), GARAMOND.clone()];
+        assert_eq_font!(fonts.font(FontId(0)), DEJA_VU);
+        assert_eq_font!(fonts.font(FontId(1)), GARAMOND);
+    }
+
+    #[test]
+    fn font_map_for_slices() {
+        let fonts: &[Font] = &[DEJA_VU.clone(), GARAMOND.clone()][..];
+        assert_eq_font!(fonts.font(FontId(0)), DEJA_VU);
+        assert_eq_font!(fonts.font(FontId(1)), GARAMOND);
+    }
+}
